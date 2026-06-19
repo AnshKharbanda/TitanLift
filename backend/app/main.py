305736a56ea1674ea48413ed4fdef9ac95719588
Ansh Kharbanda <anshkharbanda7@gmail.com>
2026-06-20 03:57:55 +0530

    Changes made

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,11 +5,20 @@ from app.database import Base,engine
 from app.models import User,WeightLog,Workout,WorkoutExercise
 from app.routers.auth import auth_router
 from app.routers.workout import workout_router
+from app.routers.weightlog import weight_log_router
+from app.routers.exercise import exercise_router
+from app.routers.workoutexercise import workout_exercise_router
+
+
 
 app=FastAPI()
 
 app.include_router(auth_router)
 app.include_router(workout_router)
+app.include_router(weight_log_router)
+app.include_router(exercise_router)
+app.include_router(workout_exercise_router)
+
 
 
 
